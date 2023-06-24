3ae3ff241f84dde14cd0d2cc4063d6bedc611b1f
wasshoy <404ari@gmail.com>
2023-06-24 14:36:11 +0900

    add some plugins and package

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("com.diffplug.spotless") version "6.19.0"
 }
 
 group = "jp.wasshoy"
@@ -16,14 +17,24 @@ repositories {
 }
 
 dependencies {
-	// FIXME: JPA のアクセスが可能になったら消す
-	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter
-	implementation("org.springframework.boot:spring-boot-starter:3.1.0")
-	// implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	// https://mvnrepository.com/artifact/org.postgresql/postgresql
+	implementation("org.postgresql:postgresql:42.6.0")
+	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+	implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+	// https://mvnrepository.com/artifact/org.projectlombok/lombok
+	compileOnly("org.projectlombok:lombok:1.18.26")
+	annotationProcessor("org.projectlombok:lombok:1.18.26")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	// https://mvnrepository.com/artifact/org.assertj/assertj-core
     testImplementation("org.assertj:assertj-core:3.24.2")
 
+}
+
+spotless {
+	java {
+		googleJavaFormat()
+	}
 }
 
 tasks.withType<Test> {
